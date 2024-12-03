f1f0a1c4e05ec40b3a7d30da54bf3ca0a882d3c2
zewdu444 <zewdu444@gmail.com>
2024-12-03 11:44:07 +0300

    change index and add vercel file

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ dotenv.config();
 
 import { sequelize } from "./src/models/index.js";
 import app from "./src/routes/route.js";
+
 const PORT = process.env.PORT || 3002;
 
-sequelize
-  .sync({ force: false })
-  .then(() => {
-    console.log("Database & tables created!");
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error("Error creating database & tables:", error);
-  });
+export default async (req, res) => {
+  try {
+    await sequelize.authenticate(); // Ensure database connection for every request
+    req.app = app; // Attach the Express app to the request
+    app(req, res); // Forward the request to your Express app
+  } catch (error) {
+    console.error("Database connection error:", error);
+    res.status(500).send("Internal Server Error");
+  }
+};
